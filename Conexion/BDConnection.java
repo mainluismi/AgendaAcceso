ed636c470d92e9d6ac919f0d96c06a6d99b3b982
Clase <Clase@DESKTOP-CN7RHLU>
2023-12-12 17:34:03 +0100

    Nombre de la base de datos agregada a la conexion

--- Conexion/BDConnection.java
+++ Conexion/BDConnection.java
@@ -6,7 +6,28 @@ import java.sql.SQLException;
 
 public class BDConnection {
 	
-	public static void main(String[] args) {
-		
-	}
+	private Connection conexion;
+	
+	// Constructor de la clase
+	public DBConnection() {
+        try {
+            // cargar el driver JDBC
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // preparar la URL de conexi�n
+            String url = "jdbc:mysql://localhost:3306/bdTrabajadores";
+
+            // conectar con la BBDD --> ponemos la url, el usuario y la contrase�a
+            conexion = DriverManager.getConnection(url, "root", "");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error en la carga del Driver");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Error conectando a la BBDD");
+            e.printStackTrace();
+        }
+    }
+	public Connection getConexion() {
+        return conexion;
+    }
 }
