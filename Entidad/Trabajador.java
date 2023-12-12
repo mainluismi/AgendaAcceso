77e09f774b748c52f5bcdf82d1dee06466615eae
donjulio14 <150954978+donjulio14@users.noreply.github.com>
2023-12-12 18:11:29 +0100

    Update Trabajador.java

--- Entidad/Trabajador.java
+++ Entidad/Trabajador.java
@@ -1,5 +1,67 @@
 package Entidad;
 
 public class Trabajador {
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private double sueldos;
+    private String fecha;
+    private String matricula;
 
+    public Personas(String dni, String nombre, String apellidos, double sueldos, String fecha, String matricula) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.sueldos = sueldos;
+        this.fecha = fecha;
+        this.matricula = matricula;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public double getSueldos() {
+        return sueldos;
+    }
+
+    public void setSueldos(double sueldos) {
+        this.sueldos = sueldos;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
 }
