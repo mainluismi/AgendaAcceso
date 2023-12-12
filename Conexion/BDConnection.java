c75dc5bf49bdfcad31a1b8778d93894badd5ca16
Clase <Clase@DESKTOP-CN7RHLU>
2023-12-12 17:23:52 +0100

    Import

--- Conexion/BDConnection.java
+++ Conexion/BDConnection.java
@@ -1,5 +1,12 @@
 package Conexion;
 
-public class BDConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class BDConnection {
+	
+	public static void main(String[] args) {
+		
+	}
 }
