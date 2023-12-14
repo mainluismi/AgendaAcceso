bf94dfaaf8358c72790161ba84aba9fc2e6fc456
mainluismi <luismisc02@gmail.com>
2023-12-14 17:46:48 +0100

    Añado vistas y main

--- Main/Main.java
+++ Main/Main.java
@@ -1,5 +1,20 @@
 package Main;
 
+import Vista.Inicio;
+
+/**
+ *
+ * @author Administrador
+ */
 public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Inicio pantalla = new Inicio();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+    }
 
 }
